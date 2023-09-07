c443b5052f73bb3b75c1a417dc8ecf0dc48485d0
Joseph Murray <josephwmurray@gmail.com>
2023-09-06 18:48:10 -0700

    first test established

--- Roshambo.Tests/ModelTests/RoshamboTests.cs
+++ Roshambo.Tests/ModelTests/RoshamboTests.cs
@@ -6,14 +6,21 @@ using System;
 namespace Roshambo.Tests
 {
   [TestClass]
-  public class TestName
+  public class Game
   {
     [TestMethod]
     public void RockPaperScissors_CorrectInput()
     {
       //Arrange
+      Game game = new Game();
       //Act
+      string played = game.InvalidGamePlayed();
+      List<string> playerInput = new List<string> { "rock", "paper", "scissors" };
       //Assert
+      foreach (var option in playerInput)
+      {
+        Assert.AreNotEqual(option, played);
+      }
     }
   }
 }
